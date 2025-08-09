70d768afa73c2221eba29cd0b087929d3f03e5c0
Riki <rmcal024@uottawa.ca>
2025-08-09 19:51:58 -0400

    SQL Sanitization

--- private/classses/Car.php
+++ private/classses/Car.php
@@ -61,7 +61,7 @@ class Car {
 
 
     public function create() {
-        $attributes = $this->attributes();
+        $attributes = $this->sanitized_attributes();
         $sql = "INSERT INTO cars (";
         $sql .= join(', ', array_keys($attributes));
         $sql .= ") VALUES ('";
@@ -99,6 +99,14 @@ class Car {
             $attributes[$column] = $this->$column;
         }
         return $attributes;
+    }
+
+    protected function sanitized_attributes() {
+        $sanitized = [];
+        foreach ($this->attributes() as $key => $value) {
+            $sanitized[$key] = self::$database->escape_string($value);
+        }
+        return $sanitized;
     }
 
 
